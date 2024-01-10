60301d791c79446f9f0ffe378e03ca288adf3b79
Garima <garimachandra149@gmail.com>
2024-01-10 15:20:43 +0530

    resolving issue#20

--- Light/game.js
+++ Light/game.js
@@ -8,7 +8,7 @@ const game = document.getElementById('game');
 const time_line = document.querySelector(".time .time_line");
 const timeText = document.querySelector(".timer .time_left_txt");
 const timeCount = document.querySelector(".timer .timer_sec");
-
+const question1 = document.getElementById('question1');
 
 var soundCorrect = new Audio("sounds/correctAns.mp3");
 var soundIncorrect = new Audio("sounds/wrongAns.mp3");
@@ -84,6 +84,7 @@ getNewQuestion = () => {
         return window.location.assign('end.html');
     }
     questionCounter++;
+    question1.innerText = `Question  ${questionCounter}`;//update question number
     progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
     //Update the progress bar
     progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
@@ -129,8 +130,9 @@ choices.forEach((choice) => {
             soundCorrect.play();
         } else {
             soundIncorrect.play();
+            display(currentQuestion.answer);
         }
-
+        setInterval(displaynone,4000);
         selectedChoice.parentElement.classList.add(classToApply);
         setTimeout(() => {
             selectedChoice.parentElement.classList.remove(classToApply);
@@ -178,7 +180,27 @@ function startTimerLine(time) {
         }
     }
 }
+let string;
+function display(string) {
+    let display = document.getElementById('display');
+    if (string == 1) { 
+    display.innerText = `Correct option : A`;
+    }
+    if (string == 2) { 
+        display.innerText = `Correct option : B`;
+    }
+    if (string == 3) { 
+        display.innerText = `Correct option : C`;
+    }
+    if (string == 4) { 
+        display.innerText = `Correct option : D`;
+        }
 
+}
+function displaynone() {
+    let display = document.getElementById('display');
+    display.innerText = `Correct option : `;
+}
 incrementScore = (num) => {
     score += num;
     scoreText.innerText = score;
