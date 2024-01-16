9374510fea2941b6a417a4435f07a390e58c8b31
Harshil Gupta <harshilgupta.2005@gmail.com>
2024-01-16 19:01:09 +0530

    Background music

---
+++ app.js
@@ -0,0 +1,19 @@
+let index = Math.floor(Math.random() * 5) + 1;
+let bgMusic = new Audio(`/assets/music_bg/${index}.mp3`);
+
+function changeMusic() {
+  bgMusic.src = `/assets/music_bg${index}.mp3`;
+  if (index <= music.length) {
+    index += 1;
+  } else {
+    index = 1;
+  }
+}
+
+window.addEventListener("load", () => {
+  bgMusic.play();
+});
+
+bgMusic.addEventListener("ended", () => {
+  changeMusic();
+});
